f4acc2d93c9ef52d82d1deb657c1ca5d2e054c70
Ian Nelson <ian@iannelson.uk>
2023-12-07 18:39:37 +0000

    Day 7

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using AdventOfCode2023.Days;
 using Autofac;
 
@@ -8,8 +9,8 @@ var days = container.Resolve<IEnumerable<IDay>>();
 var day = GetDay();
 Console.WriteLine("Day " + day.Day);
 string inputPath = $"InputFiles/{day.Day}.txt";
-Console.WriteLine("Part 1: " + day.Part1(inputPath));
-Console.WriteLine("Part 2: " + day.Part2(inputPath));
+DoPart(1, () => day.Part1(inputPath));
+DoPart(2, () => day.Part2(inputPath));
 
 IContainer BuildContainer()
 {
@@ -27,4 +28,15 @@ IDay GetDay()
 
     return days.Single(x => x.Day == int.Parse(args[0]));
 }
+
+void DoPart(int partNumber, Func<string> partFunc)
+{
+    var sw = new Stopwatch();
+    sw.Start();
+
+    var result = partFunc();
     
+    sw.Stop();
+    
+    Console.WriteLine($"Part {partNumber}: {result}  ({sw.ElapsedMilliseconds}ms) ({sw.ElapsedTicks} ticks)");
+}
